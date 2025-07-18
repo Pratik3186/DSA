61f5fc1f90c3992542d5fae6665f528284513a50
Pratik <pratikk.3186@gmail.com>
2025-07-18 19:19:26 +0530

    maxconsecutivenumber

---
+++ Array/MAx_consecutive_number.py
@@ -0,0 +1,13 @@
+def maxconsecutivenumber(arr):
+    if not arr:
+        return None # Handle empty array
+    max_count = 0
+    count = 1
+    for num in nums:
+        if num == 0:
+            count +=1
+            max_count = max(max_count, count)
+        else:
+            count = 0
+    return max_count  # Return the maximum count of consecutive zeros
+            
