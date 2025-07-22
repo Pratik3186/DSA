abcf3cd30e45bc81e3d7e6c80370b048fb0ebb80
Pratik <pratikk.3186@gmail.com>
2025-07-22 23:47:52 +0530

    Two Sum

---
+++ Array/Two-sum.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+    """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+    hashmap = {}
+    for i,num in enumerate(nums):
+        complement = target - num
+        if complement in hashmap:
+            return [hashmap[complement],i]
+        hashmap[num] = i
+    return []
